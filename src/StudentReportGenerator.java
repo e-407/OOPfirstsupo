888ccabd77de14f01ebdb8ae6f7f616eac818b3c
e-407 <dunne4079@gmail.com>
2025-11-20 20:24:59 +0000

    Setup and Q1.4

---
+++ src/StudentReportGenerator.java
@@ -0,0 +1,23 @@
+public class StudentReportGenerator {
+    public static final int DEFAULT_YEAR = 2025;
+    private String reportTitle;
+
+    public StudentReportGenerator(String reportTitle){
+        this.reportTitle = reportTitle;
+    }
+
+    public String generateReport(){
+        return reportTitle + " " + DEFAULT_YEAR;
+    }
+
+    public static void main(String args[]){
+        System.out.println("Inputting 'ReportName'\nExpecting 'ReportName 2025'");
+        StudentReportGenerator studentReportGenerator = new StudentReportGenerator("ReportName");
+        String report = studentReportGenerator.generateReport();
+        if (report.equals("ReportName 2025")){
+            System.out.println("Test successful");
+        } else {
+            System.out.println("Test failure");
+        }
+    }
+}
